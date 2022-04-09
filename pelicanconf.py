4e1ce31c070eb89b97f8d2a437cb333776d9e1ef
libMan <657483985@qq.com>
2022-04-10 00:05:30 +0800

    adjust links

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,13 +17,10 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python.org', 'https://www.python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 
